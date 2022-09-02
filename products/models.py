86815d592fc64068e3aa531b9095566d73295922
wlillisdev <williamlillis100@gmail.com>
2022-09-02 18:39:01 +0000

    edit to admin layout

--- products/models.py
+++ products/models.py
@@ -6,6 +6,12 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
     def __str__(self):
         return self.name
 
