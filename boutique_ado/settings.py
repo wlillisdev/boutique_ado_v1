09113e6bb930f2d691a03dcbaa13aa1b7184205b
wlillisdev <williamlillis100@gmail.com>
2022-09-22 17:08:19 +0000

    edit setting datbase url

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'DpipATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
